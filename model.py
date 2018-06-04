6361600f7b10bb8ad197225a717c4bcbe745aa98
Rajeev Vadhavkar <rajeev22.18@gmail.com>
2018-06-03 22:00:36 -0400

    Update model to only have one output

--- model.py
+++ model.py
@@ -1,10 +1,22 @@
 from keras.applications.mobilenet import MobileNet
+from keras.layers import Dense, Dropout, GlobalAveragePooling2D, Reshape
+from keras.models import Model
+
 import tensorflowjs as tfjs
 
-alpha = 0.25
+ALPHA = 0.75
+DROPOUT = 1e-3
 
-base_model = MobileNet(alpha=alpha)
-base_model = MobileNet(input_shape=(64,64,3), include_top=False)
+base_model = MobileNet(input_shape=(128,128,3), alpha=ALPHA, include_top=False)
+for layer in base_model.layers:
+  layer.trainable = False
 
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Reshape((int(1024 * ALPHA),), name='reshape_1')(x)
+x = Dropout(DROPOUT, name='dropout')(x)
+output = Dense(1, activation='sigmoid')(x)
+model = Model(inputs=base_model.input, outputs=output)
+model.compile(optimizer='rmsprop', loss='binary_crossentropy')
 tfjs.converters.save_keras_model(model, 'extension/model')
 
