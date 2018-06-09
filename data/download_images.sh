6f518aad1458320b53700a202ab639461614d934
Rajeev Vadhavkar <rajeev22.18@gmail.com>
2018-06-09 18:11:04 -0400

    make download_images.sh output less noisy

--- data/download_images.sh
+++ data/download_images.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cat rooster.txt hen.txt chickens.txt | xargs -P 16 wget -P chickens 
-cat animal.txt bird.txt | xargs -P 16 wget -P not_chickens 
+cat rooster.txt hen.txt chickens.txt | xargs -P 16 wget -q -P chickens 
+cat animal.txt bird.txt | xargs -P 16 wget -q -P not_chickens 
