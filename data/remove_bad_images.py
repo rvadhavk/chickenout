24eff820a4fba6d9f58f4b7693f9eaf9b8e59f17
Rajeev Vadhavkar <rajeev22.18@gmail.com>
2018-06-11 20:34:27 -0400

    update remove_bad_images.py script to try to load the image

--- data/remove_bad_images.py
+++ data/remove_bad_images.py
@@ -6,6 +6,7 @@ for directory in ('chickens', 'not_chickens'):
     img_path = os.path.join(directory, img_name)
     try:
       image = Image.open(img_path)
+      image.load()
       image.close()
     except IOError:
       os.remove(img_path)
