e7337db372a773a912bc966640a087323f9d6aea
Rajeev Vadhavkar <rajeev22.18@gmail.com>
2018-06-25 08:44:15 -0400

    Change caching to be based on src rather than content hash so cache lookup can be done in parallel with image fetching.

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,7 @@
 const modelPromise = tf.loadModel(chrome.extension.getURL('model/model.json'))
-const INPUT_WIDTH = 128
-const INPUT_HEIGHT = 128
 const MAX_CACHE_ENTRIES = 1e5
 
-// The DB used for caching classification results
+// Setup the DB used for caching classification results
 const dbPromise = new Promise((resolve, reject) => {
   const request = indexedDB.open('cache')
   request.onupgradeneeded = () => {
@@ -16,7 +14,7 @@ const dbPromise = new Promise((resolve, reject) => {
   request.onerror = reject
 })
 
-// Listener to accept classification requests from content scripts
+// Accept classification requests from the content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   getChickenProbability(request.src)
     .then(p => sendResponse({chickenProbability: p}))
@@ -24,70 +22,55 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   return true
 })
 
-// Runs classifier on passed-in ImageData
-// ImageData imgData -> Promise<Number>
-async function computeChickenProbability(imgData) {
-  const batch = tf.tidy(() => {
-    const tensor = tf.fromPixels(imgData, 3).toFloat()
-    // Normalize the image from [0, 255] to [-1, 1].
-    const offset = tf.scalar(127.5)
-    const normalized = tensor.sub(offset).div(offset)
-    return normalized.expandDims()
-  })
+// Look up classification result in cache and fallback to running classifier
+// String src -> Promise<Number>
+async function getChickenProbability(src) {
+  const cachedProbability = await cacheLookup(src)
+  if (cachedProbability !== undefined) {
+    console.log('cached: ' + src)
+    return cachedProbability
+  }
+  const probability = await computeChickenProbability(src)
+  console.log('computed: ' + src)
+  cacheStore(src, probability)
+  return probability
+}
+
+
+// Load img for given src and run the classifier on the img
+// String src -> Promise<Number>
+async function computeChickenProbability(src) {
+  const img = await loadImg(src)
   const model = await modelPromise
   return tf.tidy(() => {
+    const tensor = tf.fromPixels(img, 3).toFloat()
+    // Normalize the image from [0, 255] to [-1, 1].
+    const offset = tf.scalar(127.5)
+    const batch = tensor.sub(offset).div(offset).expandDims()
     const prediction = model.predict(batch)
     return prediction.get(0, 0)
   })
 }
 
-// Creates an img element for the given URL and returns a Promise which
-// resolves when the image loads
+// Create an img element for the given URL and return a Promise which
+// resolves when the img loads
 // String src -> Promise<HTMLImageElement>
-function loadImage(src) {
+function loadImg(src) {
   return new Promise((resolve, reject) => {
     const img = document.createElement('img')
-    img.width = INPUT_WIDTH
-    img.height = INPUT_HEIGHT
+    img.width = 128
+    img.height = 128
     img.src = src
     img.onload = event => resolve(event.target)
     img.onerror = reject
   })
 }
 
-// HTMLImageElement img -> ImageData
-function getImageData(img) {
-  const canvas = document.createElement('canvas')
-  canvas.width = INPUT_WIDTH
-  canvas.height = INPUT_HEIGHT
-  const context = canvas.getContext('2d')
-  context.drawImage(img, 0, 0, INPUT_WIDTH, INPUT_HEIGHT)
-  return context.getImageData(0, 0, INPUT_WIDTH, INPUT_HEIGHT)
-}
-
-// - Loads image at given URL
-// - Attempts to lookup classification result in content-addressed cache
-// - Falls back to running classifier with tf.js
-// - Stores classification result in cache
-// String src -> Promise<Number>
-async function getChickenProbability(src) {
-  const img = await loadImage(src)
-  const imgData = getImageData(img)
-  const cachedProbability = await cacheLookup(imgData)
-  if (cachedProbability !== undefined) {
-    console.log('cached: ' + src)
-    return cachedProbability
-  }
-  const probability = await computeChickenProbability(imgData)
-  console.log('computed: ' + src)
-  cacheStore(imgData, probability)
-  return probability
-}
-
-// Cache classification result, potentially evicting other cache entries with an LRU policy.
-// (ImageData, Number) -> Void
-async function cacheStore(imgData, probability) {
-  const digest = await crypto.subtle.digest('SHA-256', imgData.data)
+// Cache classification result, potentially evicting other cache entries with an LRU policy
+// (String src, Number probability) -> Void
+async function cacheStore(src, probability) {
+  const encoder = new TextEncoder()
+  const digest = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(src))
   const db = await dbPromise
   const cachedValue = {hash: digest, probability: probability, lastUseTime: Date.now()}
   const request = db.transaction('cache', 'readwrite')
@@ -121,8 +104,8 @@ async function maybeEvictLeastRecentlyUsed(db) {
 }
 
 // Lookup classification result, updating last-use timestamp, if present.
-async function cacheLookup(imgData) {
-  const digest = await crypto.subtle.digest('SHA-256', imgData.data)
+async function cacheLookup(src) {
+  const digest = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(src))
   const db = await dbPromise
   const objectStore = db.transaction('cache', 'readwrite').objectStore('cache')
   const cacheEntry = await new Promise((resolve, reject) => {
--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,7 +1,10 @@
 const imgSrcObserver = new MutationObserver(mutationList => {
   mutationList
     .map(mutation => mutation.target)
-    .forEach(img => img.setAttribute('chicken-out-blur', ''))
+    .forEach(img => {
+      img.setAttribute('chicken-out-blur', '')
+      scanImage(img)
+    })
 })
 
 function getImgsInTree(root) {
@@ -20,21 +23,11 @@ const documentObserver = new MutationObserver(mutationList => {
     .reduce((result, nodeList) => result.concat(nodeList), [])
   imgs.forEach(img => {
     img.setAttribute('chicken-out-blur', '')
-    imgSrcObserver.observe(img, {attributeFilter: ['src']})
+    imgSrcObserver.observe(img, {attributes: true, attributeFilter: ['src']})
+    scanImage(img)
   })
-  imgs.filter(img => img.complete).forEach(scanImage)
 })
 documentObserver.observe(document, {subtree: true, childList: true})
-
-// Why use document.addEventListener instead of img.onload?
-// Some websites such as Google Image Search have event handlers
-// which prevent load events from propagating down to their targets
-document.addEventListener('load', event => {
-  if (event.target.nodeName !== 'IMG') {
-    return
-  }
-  scanImage(event.target)
-}, true)
 
 // Map<String, Array<HTMLImageElement>>
 // Maps src to img elements with that src waiting for a classification.
@@ -42,28 +35,33 @@ document.addEventListener('load', event => {
 const imgsWaitingForClassification = new Map()
 
 function scanImage(img) {
-  if (img.currentSrc === '' || img.width === 1 || img.height === 1) {
+  if (img.src === '' || img.width === 1 || img.height === 1) {
     img.removeAttribute('chicken-out-blur')
     return
   }
-  const waiters = imgsWaitingForClassification.get(img.currentSrc)
+  const waiters = imgsWaitingForClassification.get(img.src)
   if (waiters !== undefined) {
-    // Some other image with the same currentSrc is already being classified.
+    // Some other img with the same src is already being classified.
     // Add this image to the set of imgs waiting for the result.
     waiters.push(img)
     return
   }
-  imgsWaitingForClassification.set(img.currentSrc, [img])
+  imgsWaitingForClassification.set(img.src, [img])
   const request = {
-    src: img.currentSrc
+    src: img.src
   }
   chrome.runtime.sendMessage(request, response => {
-    imgsWaitingForClassification.get(request.src).forEach(img => {
-      img.setAttribute('chicken-probability', response.chickenProbability)
-      if (request.src === img.currentSrc && response.chickenProbability < 0.5) {
-        img.removeAttribute('chicken-out-blur')
+    for (let img of imgsWaitingForClassification.get(request.src)) {
+      if (request.src !== img.src) {
+        continue
       }
-    })
+      img.setAttribute('chicken-probability', response.chickenProbability)
+      if (response.chickenProbability < 0.5) {
+        img.removeAttribute('chicken-out-blur')
+      } else {
+        img.setAttribute('chicken-out-blur', '')
+      }
+    }
     imgsWaitingForClassification.delete(request.src)
   })
 }
